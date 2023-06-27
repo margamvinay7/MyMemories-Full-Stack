fe97f1a599906a3f3b12ced5dad9d36cc82bb063
MARGAM VINAY <129204041+margamvinay7@users.noreply.github.com>
2023-06-27 07:24:16 +0530

    Update dbconifg.js

--- server/config/dbconifg.js
+++ server/config/dbconifg.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-const DATABASE="mongodb+srv://margamvinay:vinay756@vinay.tch0oea.mongodb.net/?retryWrites=true&w=majority"
+const DATABASE="mongodb+srv://margamviny:password@vinay.tch0oea.mongodb.net/?retryWrites=true&w=majority"
 
 const connectDB=()=>{
     mongoose.connect(DATABASE,{useUnifiedTopology:true,useNewUrlParser:true}
